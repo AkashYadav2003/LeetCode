cb0202721d9878f9077b6693adc5125035db8905
Akash Yadav <98077088+AkashYadav2003@users.noreply.github.com>
2025-08-02 22:27:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int nCr(int n, int r) {
+        // code here
+        long long res = 1;
+        for(int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+};
