d7f11d2b3a9bf36386577b0542bfce703119e475
Akash Yadav <98077088+AkashYadav2003@users.noreply.github.com>
2025-09-07 16:38:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (83.69%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int>arr(n, 0);
+        // int i = 0;
+        // int ele = 1;
+        // while ( i + 1 < n) {
+        //     arr[i] = ele;
+        //     arr[i + 1] = - ele;
+        //     ele++;
+        //     i = i + 2;
+        // }
+        // return arr;
         int i = 0;
+        int j = n - 1;
         int ele = 1;
-        while ( i + 1 < n) {
+        while (i < j) {
             arr[i] = ele;
-            arr[i + 1] = - ele;
+            i++;
+            arr[j] = - ele;
+            j--;
             ele++;
-            i = i + 2;
         }
         return arr;
     }
