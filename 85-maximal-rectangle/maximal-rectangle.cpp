4098f29c6b0ba5dd5c32e59f776c8258f4450591
Akash Yadav <98077088+AkashYadav2003@users.noreply.github.com>
2026-01-11 12:00:54 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (96.92%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) return 0; 
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> height(m, 0);
+        vector<int> left(m, 0);
+        vector<int> right(m, m);
+        int maxArea = 0;
+        for(int i = 0; i < n; i++) {
+            int curLeft = 0, curRight = m; 
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1'){
+                    height[j]++; 
+                }
+                else{
+                    height[j] = 0;
+                } 
+            } 
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1'){
+                    left[j] = max(left[j], curLeft);
+                }
+                else {
+                    left[j] = 0;
+                    curLeft = j + 1;
+                }
+            } 
+            for (int j = m - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1'){
+                    right[j] = min(right[j], curRight);
+                }
+                else {
+                    right[j] = m;
+                    curRight = j;
+                }
+            } 
+            for (int j = 0; j < m; j++){
+                maxArea = max(maxArea, (right[j] - left[j]) * height[j]);
+            }
+        } 
+        return maxArea;
+    }
+};
