4a9ee103bdbaddd93beb69fd88bf45f79e336b57
Akash Yadav <98077088+AkashYadav2003@users.noreply.github.com>
2026-08-03 22:07:10 +0530

    Time: 10 ms (29.79%) | Memory: 11.4 MB (38.63%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int t[501][501];
+
+    int help(vector<int> &piles, int i, int j) {
+        if (i > j) {
+            return 0;
+        }
+        if(t[i][j] != -1) {
+            return t[i][j];
+        }
+        int take_i = piles[i] + min(help(piles, i + 2, j), help(piles, i + 1, j - 1));
+        int take_j = piles[j] + min(help(piles, i, j - 2), help(piles, i + 1, j - 1));
+        return t[i][j] = max(take_i, take_j);
+    }
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        memset(t, -1, sizeof(t));
+        int sum = accumulate(begin(piles), end(piles),0);
+        int Al_score = help(piles, 0, n - 1);
+        return Al_score > sum / 2;
+    }
+};
